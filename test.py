e1d4f988f35bb858bfb142682b2b7a23bc73cc7e
Danny Sebastian <sebastian.community@gmail.com>
2020-11-23 11:05:14 +0700

    initial

---
+++ test.py
@@ -0,0 +1,84 @@
+import json
+import nltk
+import re
+import pickle
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
+
+with open('knowledge.json') as f1:
+  db = json.load(f1)
+
+with open('test.json') as f2:
+  test = json.load(f2)
+
+ignore_words = ['?', '!']
+classes = []
+words = []
+documents = [];
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
+
+for item in db['knowledge']:
+	for pattern in item['patterns']:
+		w = nltk.word_tokenize(pattern)
+		words.extend(w)
+		documents.append((w, item['class']))
+
+		if item['class'] not in classes:
+			classes.append(item['class'])
+
+words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+print(words) 
+
+classes = sorted(list(set(classes)))
+print(classes)    
+
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(classes,open('classes.pkl','wb'))
+
+#training
+training = []
+output_empty = [0] * len(classes)
+for doc in documents:
+    bag = []
+    pattern_words = doc[0]
+    pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+print("Training data created")
+
+# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+# equal to number of intents to predict output intent with softmax
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+#fitting and saving the model
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=1000, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+
+print("model created")
+
